1c837ecb2aaffc28ad712c18be75415ac311cc6d
chdbarsha704-prog <chdbarsha704@gmail.com>
2026-01-25 11:08:44 +0545

    docker-compose

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def predict_datapoint():
         print(results)
         return render_template('student.html',results=results[0])
 
-
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=8080)#docker ko host number
                    
